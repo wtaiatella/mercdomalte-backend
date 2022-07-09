49e5847433041fb009a2f754537bf9aa67b0012c
Wagner Taiatella <wtaiatella@gmail.com>
2022-07-08 21:01:03 -0400

    chore: test revision for changes in schema

--- tests/database.ts
+++ tests/database.ts
@@ -3,5 +3,5 @@ import { database } from "@src/adapters/database";
 export const clearTables = async () => {
   await database.product.deleteMany();
   await database.category.deleteMany();
-  await database.$disconnect();
+  // await database.$disconnect();
 };
--- tests/integration/products.spec.ts
+++ tests/integration/products.spec.ts
@@ -1,67 +1,70 @@
-import { createCategory, createProduct } from "@tests/factories";
-import { clearTables } from "@tests/database";
-import app from "@src/app";
-import request from "supertest";
+import { createCategory, createProduct } from '@tests/factories';
+import { clearTables } from '@tests/database';
 
-describe("Products", () => {
-  describe("GET /categories/:id/products", () => {
-    let product1;
-    let product2;
-    let categoryId: string;
+import app from '@src/app';
+import request from 'supertest';
 
-    beforeEach(async () => {
-      await clearTables();
-      const category = await createCategory({ name: "Periféricos" });
-      categoryId = category.id;
-      product1 = await createProduct({
-        name: "Mouse",
-        categoryId: category.id,
-      });
-      product2 = await createProduct({
-        name: "Teclado",
-        categoryId: category.id,
-      });
-    });
+describe('Products', () => {
+	describe('GET /categories/:id/products', () => {
+		let product1;
+		let product2;
+		let categoryId: string;
 
-    afterAll(async () => {
-      await clearTables();
-    });
+		beforeEach(async () => {
+			const category = await createCategory({ name: 'Perifericos' });
+			categoryId = category.id;
 
-    it("returns a list of products of that category in JSON ", async () => {
-      const res = await request(app)
-        .get(`/categories/${categoryId}/products`)
-        .expect(200);
+			product1 = await createProduct({
+				name: 'Mouse',
+				categoryId: category.id,
+			});
+			product2 = await createProduct({
+				name: 'Teclado',
+				categoryId: category.id,
+			});
+		});
 
-      const [item1, item2] = res.body;
+		afterEach(async () => {
+			await clearTables();
+		});
 
-      expect(item1.name).toBe("Mouse");
-      expect(item2.name).toBe("Teclado");
-    });
-  });
+		it('returns a list of products from that category', async () => {
+			const client = request(app);
+			const res = await client
+				.get(`/categories/${categoryId}/products`)
+				.expect(200);
 
-  describe("GET /products/:id", () => {});
-  let productId: string;
+			const [item1, item2] = res.body;
 
-  beforeEach(async () => {
-    await clearTables();
-    const category = await createCategory({ name: "Periféricos" });
-    const product = await createProduct({
-      name: "Mouse",
-      categoryId: category.id,
-    });
+			expect(item1.name).toBe('Mouse');
+			expect(item2.name).toBe('Teclado');
+		});
+	});
 
-    productId = product.id;
-  });
+	describe('GET /products/:id', () => {
+		let productId: string;
 
-  afterAll(async () => {
-    await clearTables();
-  });
+		beforeEach(async () => {
+			const category = await createCategory({ name: 'Perifericos' });
+			const product = await createProduct({
+				name: 'Mouse',
+				categoryId: category.id,
+			});
 
-  it("returns a product based on that ID", async () => {
-    const client = request(app);
-    const res = await client.get(`/products/${productId}`).expect(200);
+			productId = product.slug;
+		});
 
-    const item = res.body;
-    expect(item.name).toBe("Mouse");
-  });
+		afterEach(async () => {
+			await clearTables();
+		});
+
+		it('returns a single product based on that id', async () => {
+			const client = request(app);
+
+			const res = await client.get(`/products/${productId}`).expect(200);
+
+			const item = res.body;
+			expect(item.name).toBe('Mouse');
+		});
+	});
 });
