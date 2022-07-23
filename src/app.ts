243f4a3240494a2add7dc6edb7d855f9bdec19f1
Wagner Taiatella <wtaiatella@gmail>
2022-07-23 15:15:23 -0400

    feat: update aws download - working

--- src/app.ts
+++ src/app.ts
@@ -1,145 +1,159 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express';
+import { Request, Response } from 'express';
 
-import logger from "./adapters/logger";
+import logger from './adapters/logger';
 
-import categoryService from "./services/categoryService";
-import productService from "./services/productService";
-import mediaService from "./services/mediaService";
-import { sendEmail } from "./services/emailService";
+import categoryService from './services/categoryService';
+import productService from './services/productService';
+import mediaService from './services/mediaService';
+import { sendEmail } from './services/emailService';
 import {
-  s3getUploadSignedUrl,
-  s3getDownloadSignedUrl,
-} from "./services/awsService";
+	s3getUploadSignedUrl,
+	s3getDownloadSignedUrl,
+} from './services/awsService';
 
-const cors = require("cors");
+const cors = require('cors');
 
 const app = express();
 
 var corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200, // For legacy browser support
+	origin: '*',
+	optionsSuccessStatus: 200, // For legacy browser support
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.post("/uploadurl", cors(), async (req: Request, res: Response) => {
-  const { fileName } = req.body;
+app.post('/uploadurl', cors(), async (req: Request, res: Response) => {
+	const { fileName } = req.body;
 
-  console.log(req.body);
-  console.log(fileName);
-  const response = await s3getUploadSignedUrl(fileName);
-  console.log(`\nResponse returned by signed URL: ${response}\n`);
+	logger.debug(req.body);
+	logger.debug(fileName);
+	const response = await s3getUploadSignedUrl(fileName);
+	logger.debug(`\nResponse returned by signed URL: ${response}\n`);
 
-  res.json(response);
-  //res.json(fileName);
+	res.json(response);
+	//res.json(fileName);
 });
 
-app.post("/downloadurl", cors(), async (req: Request, res: Response) => {
-  const { fileName } = req.body;
+app.post('/downloadurl', cors(), async (req: Request, res: Response) => {
+	const { fileName } = req.body;
 
-  console.log(req.body);
-  console.log(fileName);
-  const response = await s3getUploadSignedUrl(fileName);
-  console.log(`\nResponse returned by signed URL: ${response}\n`);
+	logger.debug(req.body);
+	logger.debug(fileName);
+	const url = await s3getDownloadSignedUrl(fileName);
+	logger.debug(`\nResponse returned by signed URL: ${url}\n`);
 
-  res.json(response);
-  //res.json(fileName);
+	res.json(url);
+	//res.json(fileName);
 });
 
-app.get("/sendemail", async (req: Request, res: Response) => {
-  const emailTo = "asdf@asd.com";
-  const respEmail = sendEmail(emailTo);
-  res.json(respEmail);
-});
-
-app.get("/medias", async (req: Request, res: Response) => {
-  const medias = await mediaService.findMedias();
-  console.log(`aqui estão as Medias:`);
-  console.log(medias);
-  res.json(medias);
+app.get('/medias', async (req: Request, res: Response) => {
+	const medias = await mediaService.findMedias();
+	console.log(`aqui estão as Medias:`);
+	console.log(medias);
+	res.json(medias);
 });
 
 interface mediaProps {
-  title: string;
-  name: string;
-  slug: string;
-  icon: string;
-  type: string;
-  size: number;
-  categoryId: string;
+	title: string;
+	name: string;
+	slug: string;
+	icon: string;
+	type: string;
+	size: number;
+	categoryId: string;
 }
 
-app.post("/medias", async (req: Request, res: Response) => {
-  console.log(req.body);
-  const { title, name, slug, icon, type, size, categoryId } = req.body;
-  const media: mediaProps = {
-    title,
-    name,
-    slug,
-    icon,
-    type,
-    size,
-    categoryId,
-  };
+app.post('/medias', async (req: Request, res: Response) => {
+	console.log(req.body);
+	const { title, name, slug, icon, type, size, categoryId } = req.body;
+	const media: mediaProps = {
+		title,
+		name,
+		slug,
+		icon,
+		type,
+		size,
+		categoryId,
+	};
 
-  try {
-    const category = await mediaService.create(media);
-    res.json({ title });
-  } catch (err) {
-    res.status(400);
-    //res.json({ err: 'Invalid name' });
-  }
+	try {
+		const category = await mediaService.create(media);
+		res.json({ title });
+	} catch (err) {
+		res.status(400);
+		//res.json({ err: 'Invalid name' });
+	}
 });
 
-app.get("/mediascategories", async (req: Request, res: Response) => {
-  const categories = await mediaService.findCategories();
-  console.log(`aqui estão as Medias:`);
-  console.log(categories);
-  res.json(categories);
+app.get('/mediascategories', async (req: Request, res: Response) => {
+	const categories = await mediaService.findCategories();
+	console.log(`aqui estão as Medias:`);
+	console.log(categories);
+	res.json(categories);
 });
 
-app.get("/categories", async (_req: Request, res: Response) => {
-  const categories = await categoryService.find();
+//Verify if slug exist
+//medias/slug/${fileSlug}
+app.get('/medias/slug/:slug', async (req: Request, res: Response) => {
+	const fileSlug = req.params.slug;
+	console.log(fileSlug);
 
-  res.json(categories);
+	const medias = await mediaService.findSlug(fileSlug);
+	console.log(`aqui estão as Medias:`);
+	console.log(medias);
+	res.json(medias);
 });
 
-app.get("/categories/:id", async (req: Request, res: Response) => {
-  const categoryId = req.params.id;
-
-  const category = await categoryService.findOne(categoryId);
-
-  res.json(category);
+app.get('/sendemail', async (req: Request, res: Response) => {
+	const emailTo = 'asdf@asd.com';
+	const respEmail = sendEmail(emailTo);
+	res.json(respEmail);
 });
 
-app.get("/categories/:id/products", async (req: Request, res: Response) => {
-  const categoryId = req.params.id;
+//API de referência das aulas AWARI
+//Retirar e alterar os testes antes de publicar no portifólio
+app.get('/categories', async (_req: Request, res: Response) => {
+	const categories = await categoryService.find();
 
-  logger.debug(`categoryId = ${categoryId}`);
-
-  const products = await productService.find(categoryId);
-
-  res.json(products);
+	res.json(categories);
 });
 
-app.get("/products/:id", async (req: Request, res: Response) => {
-  const productId = req.params.id;
+app.get('/categories/:id', async (req: Request, res: Response) => {
+	const categoryId = req.params.id;
 
-  logger.info({ productId });
+	const category = await categoryService.findOne(categoryId);
 
-  const product = await productService.findOne(productId);
-
-  logger.info({ product });
-
-  res.json(product);
+	res.json(category);
 });
 
-app.post("/admin/categories", async (_req: Request, res: Response) => {
-  res.json({});
+app.get('/categories/:id/products', async (req: Request, res: Response) => {
+	const categoryId = req.params.id;
+
+	logger.debug(`categoryId = ${categoryId}`);
+
+	const products = await productService.find(categoryId);
+
+	res.json(products);
+});
+
+app.get('/products/:id', async (req: Request, res: Response) => {
+	const productId = req.params.id;
+
+	logger.info({ productId });
+
+	const product = await productService.findOne(productId);
+
+	logger.info({ product });
+
+	res.json(product);
+});
+
+app.post('/admin/categories', async (_req: Request, res: Response) => {
+	res.json({});
 });
 
 export default app;
--- src/services/mediaService.ts
+++ src/services/mediaService.ts
@@ -1,45 +1,55 @@
-import { type } from "os";
-import { validateLocaleAndSetLanguage } from "typescript";
-import { database } from "./../adapters/database";
+import { type } from 'os';
+import { validateLocaleAndSetLanguage } from 'typescript';
+import { database } from './../adapters/database';
 
 const findMedias = async () => {
-  const medias = await database.media.findMany({
-    include: {
-      category: true,
-    },
-  });
+	const medias = await database.media.findMany({
+		include: {
+			category: true,
+		},
+	});
 
-  return medias;
+	return medias;
 };
 
 const findCategories = async () => {
-  const categories = await database.mediaCategory.findMany({});
+	const categories = await database.mediaCategory.findMany({});
 
-  return categories;
+	return categories;
+};
+
+const findSlug = async (fileSlug: string) => {
+	const file = await database.media.findUnique({
+		where: {
+			slug: fileSlug,
+		},
+	});
+
+	return file;
 };
 
 interface mediaProps {
-  title: string;
-  name: string;
-  slug: string;
-  icon: string;
-  type: string;
-  size: number;
-  categoryId: string;
+	title: string;
+	name: string;
+	slug: string;
+	icon: string;
+	type: string;
+	size: number;
+	categoryId: string;
 }
 
 const create = async (media: mediaProps) => {
-  return database.media.create({
-    data: {
-      title: media.title,
-      name: media.name,
-      slug: media.slug,
-      icon: media.icon,
-      type: media.type,
-      size: media.size,
-      categoryId: media.categoryId,
-    },
-  });
+	return database.media.create({
+		data: {
+			title: media.title,
+			name: media.name,
+			slug: media.slug,
+			icon: media.icon,
+			type: media.type,
+			size: media.size,
+			categoryId: media.categoryId,
+		},
+	});
 };
 
-export default { findMedias, findCategories, create };
+export default { findMedias, findCategories, create, findSlug };
