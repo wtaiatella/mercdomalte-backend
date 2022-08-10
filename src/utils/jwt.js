1201d1abba3409dcadec99bd114ed875474b99ea
Wagner Taiatella <wtaiatella@gmail>
2022-08-10 10:03:25 -0400

    feat: add sftp for linode remote server

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,32 +1,33 @@
-const jwt = require('jsonwebtoken');
-const createError = require('http-errors');
-require('dotenv').config();
+import jwt from 'jsonwebtoken';
+import createError from 'http-errors';
+import process from 'dotenv/config';
 
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
-module.exports = {
-	signAccessToken(payload) {
-		return new Promise((resolve, reject) => {
-			jwt.sign({ payload }, accessTokenSecret, {}, (err, token) => {
-				if (err) {
-					reject(createError.InternalServerError());
-				}
-				resolve(token);
-			});
+const signAccessToken = (payload) => {
+	return new Promise((resolve, reject) => {
+		jwt.sign({ payload }, `${accessTokenSecret}`, {}, (err, token) => {
+			if (err) {
+				reject(new createError.InternalServerError());
+			}
+			resolve(token);
 		});
-	},
-	verifyAccessToken(token) {
-		return new Promise((resolve, reject) => {
-			jwt.verify(token, accessTokenSecret, (err, payload) => {
-				if (err) {
-					const message =
-						err.name == 'JsonWebTokenError'
-							? 'Unauthorized'
-							: err.message;
-					return reject(createError.Unauthorized(message));
-				}
-				resolve(payload);
-			});
-		});
-	},
+	});
 };
+
+const verifyAccessToken = (token) => {
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, `${accessTokenSecret}`, (err, payload) => {
+			if (err) {
+				const message =
+					err.name == 'JsonWebTokenError'
+						? 'Unauthorized'
+						: err.message;
+				return reject(new createError.Unauthorized(message));
+			}
+			resolve(payload);
+		});
+	});
+};
+
+export default { signAccessToken, verifyAccessToken };
