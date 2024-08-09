e29e959f6aa99011bfc424f1cc036154654a3feb
Wagner Taiatella <wtaiatella@gmail.com>
2024-08-09 16:21:56 -0300

    chore: update find files by user

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,93 +1,93 @@
-import { database } from '../adapters/database';
+import { database } from "../adapters/database";
 
 interface fileProps {
-	title: string;
-	name: string;
-	slug: string;
-	icon: string;
-	type: string;
-	size: number;
-	categoryId: string;
-	email: string;
+  title: string;
+  name: string;
+  slug: string;
+  icon: string;
+  type: string;
+  size: number;
+  categoryId: string;
+  email: string;
 }
 
 const findFiles = async () => {
-	const files = await database.file.findMany({});
+  const files = await database.file.findMany({});
 
-	return files;
+  return files;
 };
 
-const findByUser = async (userId: string) => {
-	const user = await database.user.findUnique({
-		where: {
-			id: userId,
-		},
-		include: { files: true },
-	});
+const findByUser = async (userEmail: string) => {
+  const user = await database.user.findUnique({
+    where: {
+      email: userEmail,
+    },
+    include: { files: true },
+  });
 
-	if (user) {
-		return user.files;
-	}
-	return [];
+  if (user) {
+    return user.files;
+  }
+  return [];
 };
 
 const findCategories = async () => {
-	const categories = await database.fileCategory.findMany({});
+  const categories = await database.fileCategory.findMany({});
 
-	return categories;
+  return categories;
 };
 
 const findSlug = async (fileSlug: string) => {
-	const file = await database.file.findUnique({
-		where: {
-			slug: fileSlug,
-		},
-	});
+  const file = await database.file.findUnique({
+    where: {
+      slug: fileSlug,
+    },
+  });
 
-	return file;
+  return file;
 };
 
 const fileDelete = async (filename: string) => {
-	const name = await database.file.delete({
-		where: {
-			name: filename,
-		},
-	});
+  const name = await database.file.delete({
+    where: {
+      name: filename,
+    },
+  });
 
-	return name;
+  return name;
 };
 
 const create = async (file: fileProps) => {
-	const { email } = file;
+  const { email } = file;
 
-	const user = await database.user.findUnique({
-		where: {
-			email,
-		},
-	});
+  const user = await database.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-	if (user) {
-		return database.file.create({
-			data: {
-				title: file.title,
-				name: file.name,
-				slug: file.slug,
-				icon: file.icon,
-				type: file.type,
-				size: file.size,
-				userId: user.id,
-			},
-		});
-	} else {
-		return ' falha de usuario';
-	}
+  if (user) {
+    return database.file.create({
+      data: {
+        title: file.title,
+        name: file.name,
+        slug: file.slug,
+        icon: file.icon,
+        type: file.type,
+        size: file.size,
+        userId: user.id,
+      },
+    });
+  } else {
+    return " falha de usuario";
+  }
 };
 
 export default {
-	findFiles,
-	findCategories,
-	create,
-	findSlug,
-	findByUser,
-	fileDelete,
+  findFiles,
+  findCategories,
+  create,
+  findSlug,
+  findByUser,
+  fileDelete,
 };
